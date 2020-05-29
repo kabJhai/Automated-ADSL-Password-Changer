cb05da1341dbaf10a97383ec20f8b4588c78e59f
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-05-29 14:25:17 +0300

    Update it to change every 24 hours only

--- automate.py
+++ automate.py
@@ -1,40 +1,61 @@
 from selenium import webdriver #Web driver implementations
 from selenium.webdriver.common.keys import Keys #Provide keys in keyboard
 import random #To generate random number
+from datetime import date
+today = str(date.today()) #Get todays date
 
-#Create the instance of firefox driver
-#You can call firefox or any other driver you have installed
-driver = webdriver.Firefox()
+previousDate = ""
+#Make sure the file exists
+try:
+    f = open('date','r')
+    previousDate = f.read()
+    f.close()
+except Exception:
+    pass
 
-#username
-username = "admin"
+#If it is the same date do not perform any operation
+if previousDate == today:
+    pass
+else:
+    #Create the instance of firefox driver
+    #You can call firefox or any other driver you have installed
+    driver = webdriver.Firefox()
 
-#password
-adslPassword = "admin"
-#Navigate to a url
-driver.get("http://"+username+":"+adslPassword+"@192.168.1.1/basic/home_wlan.htm")
+    #username
+    username = "admin"
 
-#Confirm the title
-assert "" in driver.title #You can comment this out or change it to your title of the adsl
+    #password
+    adslPassword = "admin"
+    #Navigate to a url
+    driver.get("http://"+username+":"+adslPassword+"@192.168.1.1/basic/home_wlan.htm")
 
-#Access the element by name
-elem = driver.find_element_by_name("PreSharedKey")
-password = random.randint(100000000,999999999) #Generate new 9 digit random password
-#Send the password value to the input element
-elem.send_keys(password)
+    #Confirm the title
+    assert "" in driver.title #You can comment this out or change it to your title of the adsl
 
-#Access the save button
-saveButton = driver.find_element_by_name("SaveBtn")
+    #Access the element by name
+    elem = driver.find_element_by_name("PreSharedKey")
+    password = random.randint(100000000,999999999) #Generate new 9 digit random password
+    #Send the password value to the input element
+    elem.send_keys(password)
 
-#Perform a click action
-saveButton.click()
+    #Access the save button
+    saveButton = driver.find_element_by_name("SaveBtn")
 
-#Create or edit a file named 'Generated Password.txt'
-f = open('Generated Password.txt','w')
-#Write the new password to it
-f.write("Password : "+str(password))
-#Close the file
-f.close()
+    #Perform a click action
+    saveButton.click()
 
-#Close the driver
-driver.close()
+    #Create or edit a file named 'Generated Password.txt'
+    f = open('Generated Password.txt','w')
+    #Write the new password to it
+    f.write("Password : "+str(password))
+    #Close the file
+    f.close()
+
+    #Create or edit a file named 'date'
+    f = open('date','w')
+    #Write todays date to it
+    f.write(str(today))
+    #Close the file
+    f.close()
+    #Close the driver
+    driver.close()
